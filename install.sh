8e2bcbfed6b504580d4b07698054dfb1d4844165
krnick <sungboss2004@gmail.com>
2020-12-03 11:48:38 +0800

    add script

---
+++ install.sh
@@ -0,0 +1,8 @@
+cp vimrc ~/.vimrc
+
+git config --global user.name krnick
+git config --global user.email sungboss2004@gmail.com
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.st status
