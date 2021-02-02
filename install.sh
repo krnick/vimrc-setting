ceede660ba9457d77f2f491f20033afdcdf1d127
JunWei Song <sungboss2004@gmail.com>
2021-02-02 11:03:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+brew install vim
+
 cp vimrc ~/.vimrc
 
 git config --global user.name krnick
@@ -6,3 +8,7 @@ git config --global user.email sungboss2004@gmail.com
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.st status
+
+brew install python@3.8
+brew install openjdk@8
+brew install graphviz
